c68061f838a7b0750f260cba625749f213b724ad
50488 <50488@192.168.0.19>
2024-07-18 20:08:02 -0600

    carga inicial

---
+++ claseseprogra/src/claseseprogra/Claseseprogra.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package claseseprogra;
+
+/**
+ *
+ * @author 50488
+ */
+public class Claseseprogra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
