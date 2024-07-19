80e51d80ddfb7eb554ce6b4c77592bd882255e7d
50488 <50488@192.168.0.19>
2024-07-19 10:48:26 -0600

    ciertos cambios  y inicio de cuotas

---
+++ Carranza_Esdras_Clics/src/carranza_esdras_clics/Carranza_Esdras_Clics.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carranza_esdras_clics;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 50488
+ */
+public class Carranza_Esdras_Clics {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner info = new Scanner(System.in);
+        
+        info.useDelimiter("\n");
+        
+        System.out.println("Bienvenido a la aplicacion de calculo del costo de clics");
+        System.out.println("");
+        
+    }
+    
+}
